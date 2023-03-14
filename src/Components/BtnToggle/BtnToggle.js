c952d4ce8f5e454a7a112e6f04b5b0dedf8202fa
Maikoke <mamadoumaikoke11@gmail.com>
2023-03-14 01:26:19 +0100

    some custom property and responsive

--- src/Components/BtnToggle/BtnToggle.js
+++ src/Components/BtnToggle/BtnToggle.js
@@ -9,9 +9,9 @@ export default function BtnToggle() {
     const {toggleTheme, theme} = useContext(ThemeContext);
     console.log(theme);
   return (
-    <button onClick={toggleTheme} className="button-theme">
+    <button onClick={toggleTheme} className={theme ? "button-theme dark-theme": "button-theme light-theme"}>
         {theme ? "LIGHT": "DARK"}
-        <img src={theme ? IconSun: IconMoon} alt="ccon" />
+        <img src={theme ? IconSun: IconMoon} alt="icon" />
     </button>
   )
 }
